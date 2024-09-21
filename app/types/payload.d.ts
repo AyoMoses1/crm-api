8209873f619e06ccaccd72a1f83a27490b1beaed
AyoMoses1 <ayomoses111@gmail.com>
2024-09-21 20:51:50 +0100

    resolve user payload errors

--- app/types/payload.d.ts
+++ app/types/payload.d.ts
@@ -3,13 +3,5 @@ interface UserPayload {
   last_name: string
   email: string
   phone_number: string
-  school_id?: number
   role_id: number
-  image?: string
-  gender: string
-  is_student?: boolean
-  matric_number?: string
-  department_id?: number
-  year_of_enrollment?: number
-  current_level?: string
 }
--- app/validators/registration.ts
+++ app/validators/registration.ts
@@ -4,7 +4,6 @@ export const registerUserValidator = vine.compile(
   vine.object({
     first_name: vine.string().trim().minLength(3),
     last_name: vine.string().trim().minLength(3),
-    gender: vine.enum(['male', 'female', 'other']),
     email: vine
       .string()
       .trim()
@@ -21,47 +20,10 @@ export const registerUserValidator = vine.compile(
         const user = await db.from('users').where('phone_number', value).first()
         return !user
       }),
-    school_id: vine.number().exists(async (db, value) => {
-      const school = await db.from('schools').where('id', value).first()
-      return !!school
-    }),
+
     role_id: vine.number().exists(async (db, value) => {
-      const school = await db.from('roles').where('id', value).first()
-      return !!school
+      const role = await db.from('roles').where('id', value).first()
+      return !!role
     }),
-    is_student: vine.boolean(),
-  })
-)
-
-export const registerExecutiveValidator = vine.compile(
-  vine.object({
-    first_name: vine.string().trim().minLength(3),
-    last_name: vine.string().trim().minLength(3),
-    gender: vine.enum(['male', 'female', 'other']),
-    email: vine
-      .string()
-      .trim()
-      .email()
-      .unique(async (db, value) => {
-        const user = await db.from('users').where('email', value).first()
-        return !user
-      }),
-    phone_number: vine
-      .string()
-      .trim()
-      .minLength(11)
-      .unique(async (db, value) => {
-        const user = await db.from('users').where('phone_number', value).first()
-        return !user
-      }),
-  })
-)
-
-export const studentValidator = vine.compile(
-  vine.object({
-    matric_number: vine.string().trim().minLength(3),
-    department_id: vine.number().min(1),
-    year_of_enrollment: vine.number().min(1900).max(new Date().getFullYear()),
-    current_level: vine.string().trim().minLength(1),
   })
 )
