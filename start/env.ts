7f4dc25b1666c68154ad135952669a210aa579a1
AyoMoses1 <ayomoses111@gmail.com>
2024-09-22 21:48:13 +0100

    fix: Resolve google and facebook oauth

--- start/env.ts
+++ start/env.ts
@@ -38,4 +38,14 @@ export default await Env.create(new URL('../', import.meta.url), {
   SMTP_PORT: Env.schema.string(),
   SMTP_USERNAME: Env.schema.string(),
   SMTP_PASSWORD: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring ally package
+  |----------------------------------------------------------
+  */
+  FACEBOOK_CLIENT_ID: Env.schema.string(),
+  FACEBOOK_CLIENT_SECRET: Env.schema.string(),
+  GOOGLE_CLIENT_ID: Env.schema.string(),
+  GOOGLE_CLIENT_SECRET: Env.schema.string()
 })
