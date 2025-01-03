bc4d0142069c5e844066e20b9f1cc02d866a04f7
AyoMoses1 <ayomoses111@gmail.com>
2025-01-03 21:45:32 +0100

    Fix lint errors

--- app/modules/auth/routes.ts
+++ app/modules/auth/routes.ts
@@ -6,6 +6,5 @@ router
     router.post('/verify-email', [AuthController, 'verifyEmail'])
     router.post('/set-password', [AuthController, 'setPassword'])
     router.post('/login', [AuthController, 'login'])
-    router.get('/:provider/redirect', [AuthController, 'google'])
   })
   .prefix('/api/v1/auth')
